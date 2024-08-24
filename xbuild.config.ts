02bb41499fcd5284b9cd77fc5912126467d5393a
Garefild <Garefild2@gmail.com>
2024-08-24 16:48:02 +0300

    xBuild: use 0.0.1-beta.4 as build system

--- xbuild.config.ts
+++ xbuild.config.ts
@@ -1,15 +1,29 @@
-import { version } from 'process';
+/**
+ * Import will remove at compile time
+ */
+
 import type { xBuildConfig } from '@remotex-labs/xbuild';
 
+/**
+ * Imports
+ */
+
+import { version } from 'process';
+import pkg from './package.json' with  { type: 'json' };
+
+const externals = [
+    ...Object.keys(pkg.dependencies ?? {}),
+    ...Object.keys(pkg.devDependencies ?? {}),
+];
+
 const config: xBuildConfig = {
-    declaration: false,
-    buildOnError: true,
+    declaration: true,
     esbuild: {
         bundle: true,
         minify: true,
         target: [ `node${ version.slice(1) }` ],
         platform: 'node',
-        external: [ 'esbuild', 'process' ],
+        external: externals,
         sourcemap: true,
         entryPoints: [ 'src/index.ts' ]
     }

e28218002060f9cb26e7ef0cdb466a06c766d45e
Garefild <Garefild2@gmail.com>
2024-10-28 13:30:08 +0200

    esbuild.error: add support for esBuild Message error object

--- src/errors/esbuild.error.ts
+++ src/errors/esbuild.error.ts
@@ -2,13 +2,18 @@
  * Import will remove at compile time
  */
 
-import type { Message } from 'esbuild';
+import type { Location, Message } from 'esbuild';
 
 /**
  * Imports
  */
 
+import { join } from 'path';
+import { cwd } from 'process';
+import { existsSync, readFileSync } from 'fs';
 import { BaseError } from '@errors/base.error';
+import { Colors, setColor } from '@components/colors.component';
+import { formatErrorCode, highlightCode } from '@remotex-labs/xmap';
 
 /**
  * Represents an error that occurs during the esbuild process.
@@ -17,81 +22,117 @@ import { BaseError } from '@errors/base.error';
  * It captures the error message and maintains the proper stack trace, allowing for easier debugging
  * and identification of errors that occur during the build process.
  *
- * @class esbuildError
+ * @class esBuildError
  * @extends BaseError
- *
- * @param message - An object containing the error message. The `text` property is used to set the
- * error message for the instance.
- *
- * @remarks
- * - This class is designed to handle errors specifically related to the esbuild process.
- * - A placeholder for printing the source code of the error is included for future implementation.
- * - The name of the error is set to 'EsbuildError' for clarity in error handling.
- *
- * @example
- * ```typescript
- * throw new EsbuildError({ text: 'Failed to build module.' });
- * ```
- * In this example, an instance of `EsbuildError` is thrown with a specified error message, which can
- * be caught and handled appropriately.
- *
- * @public
  */
 
 export class esBuildError extends BaseError {
+    originalErrorStack?: string;
+
     /**
      * Creates an instance of the EsbuildError class.
      *
-     * This constructor initializes the error message using the provided `message` object, captures
-     * the stack trace for better debugging, and sets the name of the error to 'EsbuildError'.
-     *
      * @param message - An object containing the error message. The `text` property is used to initialize
      * the base error class with a descriptive message about the error encountered during the esbuild process.
-     *
-     * @remarks
-     * - The constructor includes a placeholder for future implementation of printing the source code related
-     * to the error.
-     * - The stack trace is captured to maintain the context of the error's origin, allowing developers
-     * to trace back to the point of failure in the code.
-     *
-     * @public
      */
 
     constructor(message: Message) {
         super(message.text);
+        this.name = 'esBuildError';
 
         // Maintain proper stack trace
         if (Error.captureStackTrace) {
             Error.captureStackTrace(this, esBuildError);
         }
 
-        // Assign the name of the error
-        this.name = 'EsbuildError';
+        if (message.location) {
+            this.stack = this.generateFormattedError(message);
+        }
+    }
 
-        if (!message.location)
-            return;
+    /**
+     * Generates a formatted error message with highlighted code.
+     *
+     * @param message - An esbuild Message object containing error information.
+     * @returns A formatted string of the error message.
+     */
 
-        // const location = message.location;
-        // this.stackArray = this.getFormattedStackEntries([{
-        //     at: '',
-        //     line: location.line,
-        //     column: location.column,
-        //     file: location.file
-        // }], this.sourceMap);
-        //
-        // const highlightedCode = __ACTIVE_COLOR ? highlightCode(location.lineText) : location.lineText;
-        // this.blockCode = formatErrorCode({
-        //     line: location.line,
-        //     name: null,
-        //     column: location.column + 1,
-        //     source: location.file,
-        //     endLine: location.line + 1,
-        //     startLine: location.line - 1,
-        //     sourceRoot: location.file,
-        //     code: highlightedCode
-        // }, {
-        //     color: __ACTIVE_COLOR ? Colors.BrightPink : '',
-        //     reset: __ACTIVE_COLOR ? Colors.Reset : ''
-        // });
+    private generateFormattedError(message: Message): string {
+        const { text, location, notes } = message;
+        let formattedError = this.applyColor(Colors.Reset, `\n${ this.name }: ${ this.applyColor(Colors.Gray, location?.file ?? '') }\n`);
+        formattedError += this.applyColor(Colors.LightCoral, `${ text }\n\n`);
+
+        notes.forEach(note => {
+            formattedError += this.applyColor(Colors.Gray, `${ note.text }\n\n`);
+        });
+
+        if (location) {
+            const code = this.readCode(location.file);
+            if (code) {
+                formattedError += `${ this.formatCodeSnippet(code, location) }\n`;
+            }
+        }
+
+        return formattedError;
+    }
+
+    /**
+     * Reads code from a file if it exists.
+     *
+     * @param path - The file path to read from.
+     * @returns Array of lines if file exists, otherwise null.
+     */
+
+    private readCode(path: string): string[] | null {
+        try {
+            return existsSync(path) ? readFileSync(join(cwd(), path), 'utf-8').split('\n') : null;
+        } catch {
+            return null;
+        }
+    }
+
+    /**
+     * Formats a code snippet with highlighted errors.
+     *
+     * @param code - Array of code lines.
+     * @param location - The error location within the file.
+     * @returns A formatted and highlighted code snippet string.
+     */
+
+    private formatCodeSnippet(code: string[], location: Location): string {
+        const { line = 1, column = 0, file } = location;
+        const startLine = Math.max(line - 3, 0);
+        const endLine = Math.min(line + 3, code.length);
+
+        const relevantCode = highlightCode(code.slice(startLine, endLine).join('\n'));
+
+        return formatErrorCode({
+            line,
+            name: null,
+            code: relevantCode,
+            source: file,
+            endLine,
+            startLine,
+            column: column + 1,
+            sourceRoot: null,
+            sourceIndex: -1,
+            generatedLine: -1,
+            generatedColumn: -1
+        }, {
+            color: global.__ACTIVE_COLOR ? Colors.BrightPink : '',
+            reset: global.__ACTIVE_COLOR ? Colors.Reset : ''
+        });
+    }
+
+    /**
+     * Applies color to a given text if colors are enabled.
+     *
+     * @param color - The color code.
+     * @param text - The text to colorize.
+     * @returns The colorized text if colors are active, otherwise plain text.
+     */
+
+    private applyColor(color: Colors, text: string): string {
+        return global.__ACTIVE_COLOR ? setColor(color, text) : text;
     }
 }

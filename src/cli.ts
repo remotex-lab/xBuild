5b1949c59bf5f2e81e94b173b79e74b984f2b275
Garefild <Garefild2@gmail.com>
2024-10-29 04:28:54 +0200

    cli: add cli.js for cli invoke

---
+++ src/cli.ts
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+/**
+ * Import will remove at compile time
+ */
+
+import type { xBuildError } from '@errors/xbuild.error';
+import type { VMRuntimeError } from '@errors/vm-runtime.error';
+
+/**
+ * Imports
+ */
+
+import { buildWithArgv } from './index.js';
+import { bannerComponent } from '@components/banner.component';
+
+/**
+ * Banner
+ */
+
+console.log(bannerComponent());
+
+/**
+ * Run entrypoint of xBuild
+ */
+
+buildWithArgv(process.argv).catch((error: VMRuntimeError & xBuildError) => {
+    console.error(error.stack);
+});

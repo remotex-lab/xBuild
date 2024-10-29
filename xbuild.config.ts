a18679fbc222cbbf66ddb90d5d16252d3ed0a62c
Garefild <Garefild2@gmail.com>
2024-10-29 04:28:54 +0200

    cli: add cli.js for cli invoke

--- xbuild.config.ts
+++ xbuild.config.ts
@@ -16,36 +16,40 @@ import pkg from './package.json' with { type: 'json' };
  * Config build
  */
 
-const config: xBuildConfig = {
-    declaration: true,
-    define: {
-        __VERSION: pkg.version
-    },
-    esbuild: {
-        bundle: true,
-        minify: true,
-        format: 'esm',
-        target: [ `node${ version.slice(1) }` ],
-        platform: 'node',
-        packages: 'external',
-        sourcemap: true,
-        sourceRoot: `https://github.com/remotex-lab/xBuild/tree/v${ pkg.version }/`,
-        entryPoints: {
-            index: 'src/index.ts'
-        }
-    },
-    hooks: {
-        onLoad: (content: string | Uint8Array, loader: string | undefined, args: OnLoadArgs) => {
-            if (!/.css|.html/.test(args.path)) {
-                return;
+const config: xBuildConfig[] = [
+    {
+        declaration: true,
+        define: {
+            __VERSION: pkg.version
+        },
+        esbuild: {
+            bundle: true,
+            minify: true,
+            format: 'esm',
+            target: [ `node${ version.slice(1) }` ],
+            platform: 'node',
+            packages: 'external',
+            sourcemap: true,
+            external: [ './index.js' ],
+            sourceRoot: `https://github.com/remotex-lab/xBuild/tree/v${ pkg.version }/`,
+            entryPoints: {
+                cli: 'src/cli.ts',
+                index: 'src/index.ts'
             }
+        },
+        hooks: {
+            onLoad: (content: string | Uint8Array, loader: string | undefined, args: OnLoadArgs) => {
+                if (!/.css|.html/.test(args.path)) {
+                    return;
+                }
 
-            return {
-                loader: 'text',
-                contents: content
-            };
+                return {
+                    loader: 'text',
+                    contents: content
+                };
+            }
         }
     }
-};
+];
 
 export default config;
